d8935751d2de31cbc35c96b7726b5456cac4f327
suryapoojasree <suryapoojasree.h@gmail.com>
2023-06-11 18:21:12 +0530

    string concept of javascript

---
+++ string.js
@@ -0,0 +1,8 @@
+let text = "Hello world";
+let char = text.charAt(1);
+console.log(char);
+console.log(typeof text);
+console.log(typeof char);
+const text1 = text[0];
+console.log(text1);
+console.log(typeof text1);
